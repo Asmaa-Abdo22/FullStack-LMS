7bd0e91cb9b4e9787d947cd023bec20b87235c65
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2025-11-25 19:39:05 +0200

    start the app

--- client/src/Context/AppContext.js
+++ client/src/Context/AppContext.js
@@ -2,5 +2,8 @@ import { createContext } from "react";
 
 export const AppContextt = createContext();
 export const AppContexttProvider = ({ children }) => {
-  return <AppContextt.Provider>{children}</AppContextt.Provider>;
+  let value = {
+    
+  };
+  return <AppContextt.Provider value={value}>{children}</AppContextt.Provider>;
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/outfit';
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource-variable/outfit";
+import "./index.css";
+import App from "./App.jsx";
+import { AppContexttProvider } from "./Context/AppContext.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppContexttProvider>
+      <App />
+    </AppContexttProvider>
+  </StrictMode>
+);
