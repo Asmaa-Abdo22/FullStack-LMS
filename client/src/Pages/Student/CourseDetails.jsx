4ca02a363a205ddd707bf70f5a4f23a3c8732f94
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2025-12-07 23:52:45 +0200

    start coursedetails page

--- client/src/Pages/Student/CourseDetails.jsx
+++ client/src/Pages/Student/CourseDetails.jsx
@@ -2,7 +2,7 @@
 
 const CourseDetails = () => {
   return <>
-  <div className='px-7 md:px-8 lg:px-36 pt-32'>CourseDetails</div>
+  <div className='px-7 md:px-8 lg:px-36 pt-32 '>CourseDetails</div>
   </>
 }
 
